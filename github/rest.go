b3bd9d5c989c50fbab6d1762e4833093fbfcc038
arbeithand <handarbeit@handarbeit.io>
2026-04-01 11:53:40 -0400

    Add CreateDraftPR, MarkPRReady, and restPostWithResponse to GitHub client
    Adds the API layer needed for engine-managed PR lifecycle:
    - restPostWithResponse: POST with response body decoding
    - CreateDraftPR: create a draft PR linked to an issue
    - MarkPRReady: transition draft PR to ready-for-review

--- github/rest.go
+++ github/rest.go
@@ -73,6 +73,35 @@ func (c *Client) restPost(url string, body interface{}) error {
 	return c.restRequest("POST", url, body)
 }
 
+// restPostWithResponse POSTs and decodes the response body into the provided target.
+func (c *Client) restPostWithResponse(url string, body interface{}, target interface{}) error {
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return fmt.Errorf("marshaling request: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader(jsonBody))
+	if err != nil {
+		return fmt.Errorf("creating request: %w", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+c.token)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/vnd.github+json")
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("executing request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		respBody, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("GitHub API returned %d: %s", resp.StatusCode, string(respBody))
+	}
+
+	return json.NewDecoder(resp.Body).Decode(target)
+}
+
 func (c *Client) restPatch(url string, body interface{}) error {
 	return c.restRequest("PATCH", url, body)
 }
