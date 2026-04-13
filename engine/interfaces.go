646fb63c36488f5f2621e34c8969f88754f44d68
arbeithand <handarbeit@handarbeit.io>
2026-04-13 19:10:23 -0400

    Add SeedLabels to GitHubClient interface; call at engine startup
    - Add SeedLabels(owner, repo string, stageNames []string, lockedUser string) error
      to the GitHubClient interface
    - Add SeedLabels stub to mockGitHubClient and testGitHubUpgradeClient
    - Call e.client.SeedLabels() in Run() after checkStageColumnAlignment;
      failure is non-fatal (logs warning, poll loop continues)

--- engine/interfaces.go
+++ engine/interfaces.go
@@ -29,6 +29,7 @@ type GitHubClient interface {
 	MergePR(owner, repo string, prNumber int) error
 	FetchLatestRelease(owner, repo string) (*gh.LatestRelease, error)
 	FetchLabelAppliedAt(owner, repo string, issueNumber int, labelName string) (time.Time, error)
+	SeedLabels(owner, repo string, stageNames []string, lockedUser string) error
 	RateLimitStats() (rest, graphql gh.RateLimitStats)
 }
 
